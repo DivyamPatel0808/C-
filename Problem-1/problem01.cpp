ce4e078638905180ff0ea6b42f4d02caed0b2391
DivyamPatel0808 <57854368+DivyamPatel0808@users.noreply.github.com>
2020-04-26 23:42:37 +0400

    Add files via upload

---
+++ Problem-1/problem01.cpp
@@ -0,0 +1,18 @@
+/*
+
+Name : Divyam Patel
+Code : find max result, min result, average result from given the student data. 
+Date : 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+//code goes here 
+
+  return 0;
+}
